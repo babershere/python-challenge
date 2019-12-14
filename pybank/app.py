7234aed70ea567a224559027128edfb63c0e89b9
rey <rey@nakeddev.com>
2019-12-13 19:45:36 -0800

    pseudo code for pybank app

---
+++ pybank/app.py
@@ -0,0 +1,25 @@
+#import libraries needed
+
+#create path to assets
+
+#define all variables
+
+#read the cvs file in assets 
+
+#create the loop to read through all the csv data sets
+
+#create functions or define new variables that will target all the keys and values for
+
+#total number of months
+
+#net total for Profit/Losses over the entire period
+
+#average change in Profit/Losses over the entire period
+
+#greatest increase in Profit/Losses (date and amount) over the entire period
+
+#greatest decrease in losses (date and amount over the entire period)
+
+#print all reseults
+
+#export out or write to text file
