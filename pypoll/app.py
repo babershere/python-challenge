a1137d707cf48f9b9ece535fea514749e084b016
rey <rey@nakeddev.com>
2019-12-13 19:56:32 -0800

    pseudo code for pypoll in app

---
+++ pypoll/app.py
@@ -0,0 +1,21 @@
+#import libraries 
+
+#path the csv assets
+
+#define variables
+
+#read the csv file
+
+#create the loop to read through all the csv data sets
+
+#create functions or define new variables that will target all the keys and values 
+
+#total number of votes cast
+
+#list of candidates who receieved votes
+
+#percent of votes for each candidate won
+
+#winner of election based on popluar votes
+
+#print all analysis results or export out to text file
